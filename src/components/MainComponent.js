823f371c32894c66cb6f47aed99ef3f9f929a5b6
yesi.servi <yesica.serna@softtek.com>
2020-09-28 13:45:38 -0500

    feat: Single Page Application React
    feat: Add leaders, comments and update dishes, Add content to home

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,20 +3,33 @@ import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			dishes: DISHES,
+			comments: COMMENTS,
+			promotions: PROMOTIONS,
+			leaders: LEADERS,
 		};
 	}
-
 	render() {
 		const HomePage = () => {
-			return <Home />;
+			return (
+				<Home
+					dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+					promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+					leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+				/>
+			);
 		};
 		return (
 			<div>
@@ -24,6 +37,7 @@ class Main extends Component {
 				<Switch>
 					<Route path="/home" component={HomePage} />
 					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+					<Route exact path="/contactus" component={Contact} />} />
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
