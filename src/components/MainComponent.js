c5a522d7c647273ba9bf767531037a65e5b84d05
yesi.servi <yesica.serna@softtek.com>
2020-09-28 13:22:19 -0500

    feat: Add Nav Links and React Routing also Change Strict Mode to Fragment to fix warning

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
+import Home from './HomeComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -10,20 +11,21 @@ class Main extends Component {
 		super(props);
 		this.state = {
 			dishes: DISHES,
-			selectedDish: null,
 		};
 	}
 
-	onDishSelect(dishId) {
-		this.setState({ selectedDish: dishId });
-	}
-
 	render() {
+		const HomePage = () => {
+			return <Home />;
+		};
 		return (
 			<div>
 				<Header />
-				<Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-				<DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+				<Switch>
+					<Route path="/home" component={HomePage} />
+					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+					<Redirect to="/home" />
+				</Switch>
 				<Footer />
 			</div>
 		);
