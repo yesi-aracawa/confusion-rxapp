fc03f11705f1ac86d18f4d8715fd6762a9d55266
yesi.servi <yesica.serna@softtek.com>
2020-09-28 14:15:40 -0500

    feat: Make Single Web App 2
    Add BreadCrumbs, Routedishes to see each of them and fix onClick issue on MenuComponent to DishDetail

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
 	if (dish != null)
@@ -37,7 +38,7 @@ function RenderComments({ comments }) {
 	});
 	if (comments != null)
 		return (
-			<div className="col-12 col-md-5 m-1">
+			<div>
 				<h4>Comments</h4>
 				{comnts}
 			</div>
@@ -50,10 +51,24 @@ const DishDetail = (props) => {
 		return (
 			<div className="container">
 				<div className="row">
+					<Breadcrumb>
+						<BreadcrumbItem>
+							<Link to="/menu">Menu</Link>
+						</BreadcrumbItem>
+						<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+						<h3>{props.dish.name}</h3>
+						<hr />
+					</div>
+				</div>
+				<div className="row">
 					<div className="col-12 col-md-5 m-1">
 						<RenderDish dish={props.dish} />
 					</div>
-					<RenderComments comments={props.dish.comments} />
+					<div className="col-12 col-md-5 m-1">
+						<RenderComments comments={props.comments} />
+					</div>
 				</div>
 			</div>
 		);
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
+import DishDetail from './DishDetailComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
@@ -31,13 +32,26 @@ class Main extends Component {
 				/>
 			);
 		};
+
+		const DishWithId = ({ match }) => {
+			return (
+				<DishDetail
+					dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+					comments={this.state.comments.filter(
+						(comment) => comment.dishId === parseInt(match.params.dishId, 10)
+					)}
+				/>
+			);
+		};
+
 		return (
 			<div>
 				<Header />
 				<Switch>
 					<Route path="/home" component={HomePage} />
 					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-					<Route exact path="/contactus" component={Contact} />} />
+					<Route path="/menu/:dishId" component={DishWithId} />
+					<Route exact path="/contactus" component={Contact} />
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
