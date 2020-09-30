70a12c37a63714ffbc88f0b6cfd745c1ade69586
yesi.servi <yesica.serna@softtek.com>
2020-09-30 09:01:39 -0500

    feat: Add submit comment form at dishdetail
    clean code at contactComponent, Add submit coment button at comments and comment form with rating, author and comment, and make validations for author's name, and add react icons for button

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -14,19 +14,6 @@ class Contact extends Component {
 		super(props);
 
 		this.state = {
-			firstname: '',
-			lastname: '',
-			telnum: '',
-			email: '',
-			agree: false,
-			contactType: 'Tel.',
-			message: '',
-			touched: {
-				firstname: false,
-				lastname: false,
-				telnum: false,
-				email: false,
-			},
 		};
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleInputChange = this.handleInputChange.bind(this);
@@ -55,34 +42,7 @@ class Contact extends Component {
 		});
 	};
 
-	validate(firstname, lastname, telnum, email) {
-		const errors = {
-			firstname: '',
-			lastname: '',
-			telnum: '',
-			email: '',
-		};
-
-		if (this.state.touched.firstname && firstname.length < 3)
-			errors.firstname = 'First Name should be >= 3 characters';
-		else if (this.state.touched.firstname && firstname.length > 10)
-			errors.firstname = 'First Name should be <= 10 characters';
-
-		if (this.state.touched.lastname && lastname.length < 3) errors.lastname = 'Last Name should be >= 3 characters';
-		else if (this.state.touched.lastname && lastname.length > 10)
-			errors.lastname = 'Last Name should be <= 10 characters';
-
-		const reg = /^\d+$/;
-		if (this.state.touched.telnum && !reg.test(telnum)) errors.telnum = 'Tel. Number should contain only numbers';
-
-		if (this.state.touched.email && email.split('').filter((x) => x === '@').length !== 1)
-			errors.email = 'Email should contain a @';
-
-		return errors;
-	}
-
 	render() {
-		const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
 		return (
 			<div className="container">
 				<div className="row">
--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,123 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import {
+	Card,
+	CardImg,
+	CardText,
+	CardBody,
+	CardTitle,
+	Breadcrumb,
+	BreadcrumbItem,
+	Button,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	Row,
+	Col,
+	Label,
+} from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { BsPencil } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isModalOpen: false,
+		};
+		this.toggleModal = this.toggleModal.bind(this);
+		this.handleCommentForm = this.handleCommentForm.bind(this);
+	}
+
+	toggleModal() {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen,
+		});
+	}
+
+	handleCommentForm(event) {
+		this.toggleModal();
+		event.preventDefault();
+	}
+	render() {
+		return (
+			<div>
+				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+					<ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+					<ModalBody onSubmit={this.handleCommentForm}>
+						<LocalForm className="justify-content-center">
+							<Label htmlFor="rating">Rating</Label>
+							<Row className="form-group">
+								<Col>
+									<Control.select model=".rating" id="rating" name="rating" className="form-control">
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
+									</Control.select>
+								</Col>
+							</Row>
+							<Label htmlFor="author">Your Name</Label>
+							<Row className="form-group">
+								<Col>
+									<Control.text
+										model=".author"
+										id="author"
+										name="author"
+										placeholder="Your Name"
+										className="form-control"
+										validators={{
+											required,
+											minLength: minLength(3),
+											maxLength: maxLength(15),
+										}}
+									/>
+									<Errors
+										className="text-danger"
+										model=".author"
+										show="touched"
+										messages={{
+											required: 'Required ',
+											minLength: 'Your Name must be greater than 2 characters',
+											maxLength: 'Your Name must be 15 characters or less',
+										}}
+									/>
+								</Col>
+							</Row>
+							<Label htmlFor="comment">Comment</Label>
+							<Row className="form-group">
+								<Col>
+									<Control.textarea
+										model=".comment"
+										id="comment"
+										name="comment"
+										rows="6"
+										className="form-control"
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Col>
+									<Button type="submit" color="primary">
+										Submit
+									</Button>
+								</Col>
+							</Row>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+				<Button outline onClick={this.toggleModal} color="secondary">
+					<BsPencil /> Submit Comment
+				</Button>
+			</div>
+		);
+	}
+}
 
 function RenderDish({ dish }) {
 	if (dish != null)
@@ -41,6 +158,7 @@ function RenderComments({ comments }) {
 			<div>
 				<h4>Comments</h4>
 				{comnts}
+				<CommentForm />
 			</div>
 		);
 	else return <div>Estoy solo :c</div>;
